829b6f13c79053daa1952326a0a14bf77ef48ba5
einfalles <dn5jg@virginia.edu>
2018-05-05 12:52:47 +0200

    added basic liking mechanism

--- docs/assets/js/home.js
+++ docs/assets/js/home.js
@@ -11,6 +11,44 @@ document.addEventListener('DOMContentLoaded',function(event){
     visit_log = sanitized_date(visit_log);
   }
   localStorage.setItem('visit_log',visit_log);
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC2KlDV4DD-gR4iXLRb05aMYRvKbD3VTfM",
+    authDomain: "utility-like-system.firebaseapp.com",
+    databaseURL: "https://utility-like-system.firebaseio.com",
+    projectId: "utility-like-system",
+    storageBucket: "",
+    messagingSenderId: "830127512790"
+  };
+  firebase.initializeApp(config);
+  var current_path = window.location.pathname.split('/');
+  if (  current_path[1] == "project"  ) {
+    var clicks = 0;
+    var firebase_url = "https://utility-like-system.firebaseio.com/project-"+ current_path[2] + ".json";
+    $.get(firebase_url).done(function(data){
+      if (  data != null  ) {
+        $('.m-projectContent__count').text(data['likes']);
+      }
+    });
+    $('.m-projectContent__submit').on('click',function(){
+      clicks = clicks + 1;
+      $.get(firebase_url).done(function(data){
+        var likes = 0;
+        if (  data != null  ) {
+          likes = clicks + data['likes'];
+        } else {
+          likes = clicks;
+        }
+        $('.m-projectContent__count').text(likes);
+        $.ajax({
+          method: "PATCH",
+          url: firebase_url,
+          data: JSON.stringify({"likes":likes})
+        }).done(function( msg ){console.log('Thanks for your kudos.');});
+      });
+    })
+  }
 });
 
 
@@ -24,3 +62,37 @@ function sanitized_date(visit_log) {
   visit_log.push(d);
   return JSON.stringify(visit_log);
 }
+
+function seed_projects(){
+var projects = ['gimmegear',
+ 'walkback',
+ 'sunsama',
+ 'pom',
+ 'tunesmash',
+ 'firefly',
+ 'siempo',
+ 'jiffy',
+ 'nozzle',
+ 'tiptone',
+ 'shoshanna',
+ 'snowrva',
+ 'oesh',
+ 'latch',
+ 'teastick',
+ 'mother',
+ 'molly',
+ 'reachd',
+ 'jeffersat'];
+ for (i = 0; i < projects.length; i++) {
+   var firebase_url = "https://utility-like-system.firebaseio.com/project-"+ projects[i] + ".json";
+   var seed_like = {'likes':1};
+   console.log(firebase_url)
+   // $.post(firebase_url, JSON.stringify(seed_like));
+   $.ajax({
+     method: "PATCH",
+     url: firebase_url,
+     data: JSON.stringify(seed_like)
+   }).done(function( msg ){console.log('Thanks for your kudos.');});
+   setTimeout(function(){}, 2000);
+ }
+ }
